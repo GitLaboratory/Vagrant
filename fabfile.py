3c84b07d3ff036db87e35064a45c24aad8b5b4ee
Il'ya A. Lykov <lykov1992@gmail.com>
2013-09-08 03:16:33 +0800

    Update fabfile.py

--- fabfile.py
+++ fabfile.py
@@ -71,7 +71,7 @@ def setup_audio():
 	sudo('echo "# Link: http://www.sourcefabric.org/" >> "/etc/apt/sources.list"')
 	sudo('echo "deb http://apt.sourcefabric.org/ precise main" >> "/etc/apt/sources.list"')
 	sudo('apt-get --yes update')
-	sudo('apt-get --yes install sourcefabric-keyring')
+	sudo('apt-get --force-yes -y install sourcefabric-keyring')
 	sudo('apt-get --yes update')
 	sudo('apt-get --yes install airtime')
 
